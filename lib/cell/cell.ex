e5b92daed7949f4da4578f2d0906c266bf2e9bc9
Rage Gage <ragegage@Rages-MacBook-Air.local>
2017-03-02 12:10:14 -0800

    basic cell loop set!

---
+++ lib/cell/cell.ex
@@ -0,0 +1,49 @@
+defmodule Gol.Cell do
+  @moduledoc """
+  Each cell keeps track of its position and its alive? property (true or false)
+  """
+  alias Gol.Cell
+  defstruct [alive?: nil, position: {nil, nil}]
+
+  @doc """
+  creates a new cell
+  example usage: 
+  pid = spawn(fn -> Gol.Cell.new(%{position: {0,0}, alive?: true}) end)
+  or
+  pid2 = spawn(Gol.Cell, :new, [%{position: {0,0}, alive?: true}])
+  """
+  def new(%{position: _position, alive?: _alive?} = values) do
+    # struct(%Cell{}, values)
+    loop(struct(%Cell{}, values))
+  end
+
+  defp loop(self) do
+    receive do
+      {board, from} ->
+        new_self = board
+        # |> Helper.relevant_cells(position)
+        # |> Enums.reduce(fn cell -> cell.alive? end)
+        |> assess(self)
+        |> (&(send(from, IO.inspect(&1)))).()
+        loop(new_self)
+    end
+  end
+
+  @doc """
+  sets alive? based on count 
+  """
+  def assess(count, self) when count < 2 do
+    %{self | alive?: false}
+  end
+  def assess(count, self) when count > 3 do
+    %{self | alive?: false}
+  end
+  def assess(count, self) when count == 3 do
+    # live and also rebirth
+    %{self | alive?: true}
+  end
+  def assess(count, self) do
+    # live but no rebirth
+    self
+  end
+end
